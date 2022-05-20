b32db7710b052fb33e6ea5e23b085580d87a5b45
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-05-19 23:24:58 -0500

    feat: Header en todas las rutas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const Home = () => {
     return (
-        <div>
-            <h1>React Shop</h1>
-        </div>
+        <Header />
     );
-}
+}   
 
 export default Home;
