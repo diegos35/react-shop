cca686845bee5fb0a720569ab7797bfb9331908b
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2022-06-02 22:33:49 -0500

    feat: Custom hooks :trabajando primera parte wind

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,20 +1,12 @@
 import React, {useEffect, useState } from 'react';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
-import axios from 'axios';
+//import axios from 'axios';
+import useGetProducts from '../hooks/useGetProducts';
 
 const API = 'https://api.escuelajs.co/api/v1/products';
 const ProductList = () => {
-	const [products, setProducts] = useState([]);
-	useEffect(() => {
-        async function fetchData() {
-            const response = await axios.get(API);
-            setProducts(response.data);
-        }
-        fetchData();
-    }, []); 
-
-
+	const products = useGetProducts(API);
 	return (
 		<section className="main-container">
 			<div className="ProductList">
---
+++ src/hooks/useGetProducts.js
@@ -0,0 +1,19 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
+//funcion que vamos a exportar
+const useGetProducts = (API) =>{
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await axios.get(API);
+            setProducts(response.data);
+        }
+        fetchData();
+    }, []);
+
+    return products;     
+};
+
+export default useGetProducts;
