9ccf4d75124f8eee3db389806ffef49f5f20bb04
Diego Fernando Caviedes <diego_1996.c.c@hotmail.com>
2022-06-04 00:59:17 -0500

    feat: calculando el precio total

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -3,15 +3,15 @@ import '../styles/OrderItem.scss';
 import closeImage from '@icons/icon_close.png'
 import AppContext from '../context/AppContext';
 
-const OrderItem = () => {
+const OrderItem = ({product}) => {
 	const {state} = useContext(AppContext); //obtnemos el state 
 	return (
 		<div className="OrderItem">
 			<figure>
-				<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
+				<img src={product.images[0]} alt={product.title} />
 			</figure>
-			<p>Bike</p>
-			<p>$30,00</p>
+			<p>{product.title}</p>
+			<p>{product.price}</p>
 			<img src={closeImage} alt="close" />
 		</div>
 	);
